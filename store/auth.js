282ca55ccc544979640bf188e1ab9bc21792a8d2
Nikita Makarin <zn.makarin@gmail.com>
2020-04-13 21:19:39 +0300

    Change login action in store/auth

--- store/auth.js
+++ store/auth.js
@@ -2,42 +2,57 @@ import * as cookies from '~/plugins/vendor/cookie-universal-nuxt.js'
 import * as api from '~/api/index'
 
 export const state = () => ({
-  authorized: false
+  authorized: false,
+  token: ''
 })
 
 export const mutations = {
   SET_AUTHORIZED (state, value) {
     state.authorized = value
+  },
+  SET_TOKEN (state, token) {
+    state.token = token
   }
 }
 
 export const actions = {
-  login ({ commit }, { email, password }) {
-    api.login({ email, password })
+  async login ({ commit }, { email, password }) {
+    const response = await api.login({ email, password })
       .catch((err) => {
         console.log(err)
       })
-      .then(() => {
-        commit('SET_AUTHORIZED', true)
-        this.app.$cookies.set(cookies.auth.name, true, {
-          maxAge: cookies.auth.maxAge
-        })
-      })
+    if (!response || response.status !== 200) { return }
+    commit('SET_AUTHORIZED', true)
+    commit('SET_TOKEN', response.data.token)
+    this.app.$cookies.set(cookies.auth.name, true, {
+      maxAge: cookies.auth.maxAge
+    })
+    this.app.$cookies.set(cookies.token.name, response.data.token, {
+      maxAge: cookies.token.maxAge
+    })
   },
   setAuthorized ({ commit }, value) {
     commit('SET_AUTHORIZED', value)
+  },
+  setToken ({ commit }, token) {
+    commit('SET_TOKEN', token)
   },
   logout ({ commit }) {
     commit('SET_AUTHORIZED', false)
     this.app.$cookies.remove(cookies.auth.name)
   },
-  prolongAuth () {
+  prolongAuthCookies () {
     this.app.$cookies.set(cookies.auth.name, true, {
       maxAge: cookies.auth.maxAge
     })
+    this.app.$cookies.set(cookies.token.name,
+      this.app.$cookies.get(cookies.token.name), {
+        maxAge: cookies.token.maxAge
+      })
   }
 }
 
 export const getters = {
-  authorized: state => state.authorized
+  authorized: state => state.authorized,
+  token: state => state.authorized
 }
