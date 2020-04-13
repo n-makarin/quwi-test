33c81368ae4396eef6739fd8cdf2a5e0397f4d2f
Nikita Makarin <zn.makarin@gmail.com>
2020-04-13 22:07:33 +0300

    Add Project to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="index">
-    index
+    <div class="index__project-list">
+      <Project
+        v-for="project in projectList"
+        :key="project.id"
+        :data="project"
+        class="index__project"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import Project from '~/components/Project.vue'
 
 export default {
   components: {
+    Project
   },
   middleware: 'auth',
   computed: {
@@ -19,7 +28,7 @@ export default {
       return this.$cookies.get('token')
     }
   },
-  mounted () {
+  beforeMount () {
     this.getProjectList(this.token)
   },
   methods: {
@@ -31,4 +40,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.index {
+  &__project-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+  }
+  &__project {
+    flex-grow: 1;
+    flex-shrink: 0;
+    width: 100%;
+    max-width: 700px;
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
 </style>
