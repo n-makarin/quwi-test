379c00566dd533b87e166e49ed9dd4752db0f24b
Nikita Makarin <zn.makarin@gmail.com>
2020-04-14 12:44:00 +0300

    Add layouts/error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error">
+    <h1 v-if="error.statusCode === 404">
+      Page not found
+    </h1>
+    <h1 v-else>
+      An error occurred
+    </h1>
+    <nuxt-link to="/">
+      Home page
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+  layout: 'default'
+}
+</script>
+
+<style lang="scss" scoped>
+.error {
+  padding: 20px;
+  text-align: center;
+}
+</style>
